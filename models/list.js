1dc2f5b5912a88123b9878493db78a2ccc0fdcdf
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-17 13:50:30 -0400

    Add button to submit

--- models/list.js
+++ models/list.js
@@ -2,9 +2,17 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema
 
+// const contentSchema = new Schema({
+//   content: String,
+//   quantity: { type: Number, min: 1, max: 10, default: 1 }
+// })
+
 const listSchema = new Schema({
-  grocery: String,
-  checkbox: Boolean,
+  title: {
+    type: String,
+    required: true,
+  },
+  // content: [contentSchema],
   owner: {type: Schema.Types.ObjectId, ref: "Profile"}
 })
 
